5444f7d26ccd68374f47462cf32277459c0e29d6
DirePantsJD <20JDuarte00@gmail.com>
2023-02-05 19:26:15 +0000

    dumping into this repo my solutions to rustlings

---
+++ clippy/clippy3.rs
@@ -0,0 +1,24 @@
+// clippy3.rs
+// Here's a couple more easy Clippy fixes, so you can see its utility.
+
+use std::mem::swap as swap;
+
+#[allow(unused_variables, unused_assignments)]
+fn main() {
+    let my_option: Option<()> = None;
+
+    let my_arr = &[
+        -1, -2, -3,
+        -4, -5, -6
+    ];
+    println!("My array! Here it is: {my_arr:?}");
+
+    let my_empty_vec:Vec<u8> = vec![];
+    println!("This Vec is empty, see? {my_empty_vec:?}");
+
+    let mut value_a = 45;
+    let mut value_b = 66;
+    // Let's swap these two!
+    swap(&mut value_a,&mut value_b);
+    println!("value a: {value_a}; value b: {value_b}");
+}
---
+++ clippy/target/debug/deps/clippy3-fca79c0db803bcb3.d
@@ -0,0 +1,8 @@
+/Users/josealves/rustlings/exercises/clippy/target/debug/deps/clippy3-fca79c0db803bcb3.rmeta: clippy3.rs Cargo.toml
+
+/Users/josealves/rustlings/exercises/clippy/target/debug/deps/clippy3-fca79c0db803bcb3.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
